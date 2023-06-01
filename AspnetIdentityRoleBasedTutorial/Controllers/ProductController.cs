e648b11482c543594433cdab1e07943ea2e894d8
DBCart-ltd <aravindhkumar@dbcartindia.com>
2023-06-01 11:20:53 +0530

    GoceryDb

--- AspnetIdentityRoleBasedTutorial/Controllers/ProductController.cs
+++ AspnetIdentityRoleBasedTutorial/Controllers/ProductController.cs
@@ -4,56 +4,63 @@ using OnlineShoppingProject.BAL;
 
 namespace AspnetIdentityRoleBasedTutorial.Controllers
 {
-    //[Authorize]
-    public class ProductController : Controller
+  //[Authorize]
+  public class ProductController : Controller
+  {
+
+    private ProductImplemenationBAL productimplemenationbal;
+    public ProductController(ProductImplemenationBAL productimplemenation)
+    {
+      this.productimplemenationbal = productimplemenation;
+    }
+    public async Task<IActionResult> ProductDetails()
     {
 
-        private ProductImplemenationBAL productimplemenationbal;
-        public ProductController(ProductImplemenationBAL productimplemenation)
-        {
-            this.productimplemenationbal = productimplemenation;
-        }
-        public async Task<IActionResult> ProductDetails()
-        {
-            
-           return View(await productimplemenationbal.GetProductList());
-        }
-        
-        //Product Category - Appliances
-        public async Task<IActionResult> ProductCategory()
-        {
-            return View(await productimplemenationbal.GetProductCategoryList());
-        }
-
-        //Product Category - Mobiles
-        public async Task<IActionResult> ProductCategoryMobile()
-        {
-            return View(await productimplemenationbal.GetProductCategoryMobileList());
-        }
-
-        public IActionResult GetAll()
-        {
-            return View();
-        }
-
-        public IActionResult fashion()
-        {
-            return View();
-        }
-
-        public IActionResult groceries()
-        {
-            return View();
-        }
-
-        public IActionResult travel()
-        {
-            return View();
-        }
-
-        public async Task<IActionResult> GetListfashion(int id)
-        {
-            return View(await productimplemenationbal.GetFashionList(id));
-        }
+      return View(await productimplemenationbal.GetProductList());
     }
+
+    //Product Category - Appliances
+    public async Task<IActionResult> ProductCategory()
+    {
+      return View(await productimplemenationbal.GetProductCategoryList());
+    }
+
+    //Product Category - Mobiles
+    public async Task<IActionResult> ProductCategoryMobile()
+    {
+      return View(await productimplemenationbal.GetProductCategoryMobileList());
+    }
+
+    public IActionResult GetAll()
+    {
+      return View();
+    }
+
+    public IActionResult fashion()
+    {
+      return View();
+    }
+
+    public IActionResult groceries()
+    {
+      return View();
+    }
+
+    //Grocery From Database
+
+    public async Task<IActionResult> groceriesDb()
+    {
+      return View(await productimplemenationbal.GetGroceryListBAL());
+    }
+
+    public IActionResult travel()
+    {
+      return View();
+    }
+
+    public async Task<IActionResult> GetListfashion(int id)
+    {
+      return View(await productimplemenationbal.GetFashionList(id));
+    }
+  }
 }
